74708757cff8e1028daaf2a476d35d8b6f8cbab8
Arpad <solar@hu.inter.net>
2021-04-28 23:08:17 +0200

    feladat1 leiras

--- JS-task/task01-algorithm/main.js
+++ JS-task/task01-algorithm/main.js
@@ -10,6 +10,19 @@ const customerList = [{ "id": 1, "name": "Janeva Ciccone", "email": "jciccone0@i
 { "id": 10, "name": "Lin Surmon", "email": "lsurmon9@myspace.com", "rating": 3, "address": "4949 Memorial Way" }
 ];
 
+/**
+ * 
+ * @param {A következőképpen kell működnie a függvénynek:
+          három paramétert vár: vásárlók listája, név, értékelés
+           azoknak a customer -eknek a nevét (CSAK A NEVÉT) kell 
+           visszaadnia egy tömbben, 
+           amelyek name tulajdonságában szerepel a kapott szövegrészlet 
+           és rating -juk LEGALÁBB annyi mint a kapott rating.} list 
+ * @param {*} name 
+ * @param {*} rating 
+ * @returns 
+ */
+
 //const customerFilter = (list, name, rating) => {
 //    return list.filter( c => c.name.includes(name) && c.rating >= rating )
 //        .map( c => c.name );
